0d834d3cda800a12a3ef900e536dec82b9ec9c05
NeimarS <neimarsenac@gmail.com>
2020-10-17 21:38:17 -0300

    t

--- index.php
+++ index.php
@@ -1,5 +1,31 @@
 <?php
     $dbopts = parse_url(getenv('DATABASE_URL'));
-    var_dump($dbopts);
+    
+    $app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
+               array(
+                'pdo.server' => array(
+                   'driver'   => 'pgsql',
+                   'user' => $dbopts["user"],
+                   'password' => $dbopts["pass"],
+                   'host' => $dbopts["host"],
+                   'port' => $dbopts["port"],
+                   'dbname' => ltrim($dbopts["path"],'/')
+                   )
+               )
+    );
+
+    $app->get('/db/', function() use($app) {
+        $st = $app['pdo']->prepare('SELECT nome FROM cliente');
+        $st->execute();
+        $vencimento = $st->fetchAll(PDO::FETCH_ASSOC|PDO::FETCH_PROPS_LATE);
+        
+        foreach ($vencimento as $row){
+            foreach ($row as $campo => $nome){
+               print($nome);
+            }
+        } 
+        
+      });
+
 
 ?>
