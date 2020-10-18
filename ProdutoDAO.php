ab0fb9492eea636aef5acb21167fdebbaccd1d74
NeimarS <neimarsenac@gmail.com>
2020-10-17 23:37:44 -0300

    t

--- ProdutoDAO.php
+++ ProdutoDAO.php
@@ -13,7 +13,7 @@ class ProdutoDAO {
           
             
             try{
-                $pdo = new PDO("mysql:host=$servername;port=$porta;dbname=$databasename", $username, $password);
+                $pdo = new PDO("pgsql:host=$servername;port=$porta;dbname=$databasename", $username, $password);
                 // set the PDO error mode to exception
                 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);   
                 $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
