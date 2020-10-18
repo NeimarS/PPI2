939dc39844844021783e4373ec340de99d547595
NeimarS <neimarsenac@gmail.com>
2020-10-17 21:48:18 -0300

    t

--- index.php
+++ index.php
@@ -1,19 +1,7 @@
 <?php
     $dbopts = parse_url(getenv('DATABASE_URL'));
     
-    $app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
-               array(
-                'pdo.server' => array(
-                   'driver'   => 'pgsql',
-                   'user' => $dbopts["user"],
-                   'password' => $dbopts["pass"],
-                   'host' => $dbopts["host"],
-                   'port' => $dbopts["port"],
-                   'dbname' => ltrim($dbopts["path"],'/')
-                   )
-               )
-               
-    );
+
     var_dump($dbopts);
     /*
     $app->get('/db/', function() use($app) {
