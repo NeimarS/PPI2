4332333a48d2cc0e2b87c3595820d79aebbb5087
NeimarS <neimarsenac@gmail.com>
2020-11-20 22:57:34 -0200

    html

--- js/dadoslogin.js
+++ js/dadoslogin.js
@@ -25,7 +25,7 @@ function enviarLogin(login){
     xhttp.setRequestHeader("Content-Type","application/json");
     xhttp.send(JSON.stringify(login));   
     
-    if (cont == 3) {
+    if (cont == 9) {
         alert("Login ou senha inválidos!");
         limparFormulario();
     }
